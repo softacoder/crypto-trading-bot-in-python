45ee7d396029951316cfaf65d6f39afb453567b1
softacoder <113185323+softacoder@users.noreply.github.com>
2024-06-03 17:24:02 +0100

    commit end entry point | logger

--- TradingBotCourse/binance_futures.py
+++ TradingBotCourse/binance_futures.py
@@ -1,3 +1,4 @@
-import logging 
+import logging
+
 
 logger = logging.getLogger()
--- TradingBotCourse/main.py
+++ TradingBotCourse/main.py
@@ -21,11 +21,7 @@ file_handler.setLevel(logging.DEBUG)
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 
-logger.debug("This message is important only when debugging the program")
-logger.info("This message just shows basic information")
-logger.warning("This message is about something you should pay attention to")
-logger.error("This message helps to debug an error that occurred in your program")
 
-
-root = tk.Tk()
-root.mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.mainloop()
