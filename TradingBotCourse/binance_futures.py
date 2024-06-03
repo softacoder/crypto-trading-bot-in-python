f5d5703118c9fc302d2621b4391c2974a9754b4f
softacoder <113185323+softacoder@users.noreply.github.com>
2024-06-03 20:51:27 +0100

    commit interact with public api

--- TradingBotCourse/binance_futures.py
+++ TradingBotCourse/binance_futures.py
@@ -1,4 +1,21 @@
 import logging
-
+import requests
 
 logger = logging.getLogger()
+
+
+def get_contracts():
+
+    response_object = requests.get("https://fapi.binance.com/fapi/v1/exchangeInfo")
+    print(response_object.status_code)
+    pprint.pprint(response_object.json()['symbols'])
+
+    for contract in response_object.json()['symbol']:
+        pprint.pprint(contract)
+        print(contract['pair'])
+        contract.append(contract['pair'])
+
+    return contracts
+
+print(get_contracts())
+
