00c7a6604c0cbd782760128ab882dca956eed75a
softacoder <113185323+softacoder@users.noreply.github.com>
2024-06-04 12:41:44 +0100

    commit rest ai coding and testing

--- binance_futures.py
+++ binance_futures.py
@@ -1,21 +1,70 @@
 import logging
 import requests
 
+
 logger = logging.getLogger()
 
+class BinanceFuturesClient:
+    def __init__(self, testnet):
+        if testnet:
+            self.base_url = "https://testnet.binancefutures.com"
+        else:
+            self.base_url = "https://fapi.binance.com"
 
-def get_contracts():
+        self.prices = dict()
 
-    response_object = requests.get("https://fapi.binance.com/fapi/v1/exchangeInfo")
-    print(response_object.status_code)
-    pprint.pprint(response_object.json()['symbols'])
+        logger.info("Binance Futures Client successfully initialized")
 
-    for contract in response_object.json()['symbol']:
-        pprint.pprint(contract)
-        print(contract['pair'])
-        contract.append(contract['pair'])
+        def make_requests(self, method, endpoint, data):
+            if method == "GET":
+                response = requests.get(self.base_url + endpoint, params=data)
+            else: 
+                raise ValueError()
+            
+            if response.status_code == 200:
+                return response.json()
+            else:
+                logger.error("Error while making %s request to %s: %s (error code %s)",
+                             method, endpoint, response.json(), response.status_code)
+                return None
 
-    return contracts
+        def get_contracts(self):
+            exchange_info = self.make_request("GET", "/fapi/v1/exchangeInfo", None)
+            
+            contracts = dict()
 
-print(get_contracts())
+            if exchange_info is not None:
+                for contract_data in exchange_info['symbols']:
+                    contracts[contract_data['pair']] = contract_data
+            
+            return contracts
+        
+        def get_historical_candles(self):
+            data = dict()
+            data['symbol'] = symbol
+            data['interval'] = interval
+            data['limit'] = 1000
+
+            raw_candles - self.make_request("GET", "/fapi/v1/klines", data)
+
+            candles = []
+
+            if raw_candles is not None:
+                for c in raw_candles:
+                    candles.append([c[0], float(c[1]), float(c[2]), float(c[3]), float(c[4]), float(c[5])])
+            return
+        
+        def get_bid_ask(self, symbol):
+            data = dict()
+            data['symbol'] = symbol
+            ob_data = self.make_request("GET", "/fapi/v1/ticker/bookTicker", data)
+
+            if ob_data is not None:
+                if symbol not in self.prices:
+                    self.prices[symbol] = {'bid': float(ob_data['bidPrice']), 'ask':float(ob_data['askPrice'])}
+                else:
+                    self.prices[symbol]['bid'] = float(ob_data['bidPrice'])
+                    self.prices[symbol]['ask'] = float(ob_data['askPrice'])
+            
+            return self.prices[symbol]
 
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 import logging
 # from binance_futures import write_log
-from bitmex import get_contracts
+# from bitmex import get_contracts
+
+from connectors.binance_futures import BinanceFuturesClient
 
 logger = logging.getLogger()
 
@@ -18,31 +20,14 @@ file_handler = logging.FileHandler('info.log')
 file_handler.setFormatter(formatter)
 file_handler.setLevel(logging.DEBUG)
 
-
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 
 
 if __name__ == '__main__':
 
-    bitmex_contracts = get_contracts()
+    binance = BinanceFuturesClient(True)
+    print(binance.get_historical_candles("BTCUSDT", "1h"))
 
     root = tk.Tk()
-    root.configure(bg="gray12")
-
-    i = 0
-    j = 0
-
-    calibri_font = ("Calibri", 11, "normal")
-
-    for contract in bitmex_contracts:
-        label_widget = tk.Label(root, text=contract, bg='gray12', fg='SteelBlue1', width=13, font=calibri_font)
-        label_widget.grid(row=1, column=j, sticky='ew')
-
-        if i == 4:
-            j += 1
-            i = 0
-        else:
-            i += 1
-
     root.mainloop()
