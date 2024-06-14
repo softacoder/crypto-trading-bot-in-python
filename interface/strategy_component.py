e8160324e3183aa70c9a7fd4ad5522d7eeabd473
softacoder <113185323+softacoder@users.noreply.github.com>
2024-06-14 16:20:30 +0100

    commit strat initialization

---
+++ interface/strategy_component.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+import typing
+
+from interface.styling import *
+
+class StrategyEditor(tk.Frame):
+    def __init__(self, *args, **kwargs)
+        super().__init__(*args, **kwargs)
+
+        self._commands_frame = tk.Frame(self, bg=BG_COLOR)
+        self._table.pack(side=tk.TOP)
+
+        self._table_frame = tk.Frame(self, bg=BG_COLOR)
+        self._table_frame.pack(side=tk.TOP)
+
+        self._add_button = tk.Button(self._commands_frame, text="Add strategy", font=GLOBAL_FONT, command=self._add_strategy_row, bg=BG_COLOR_2, fg=FG_COLOR)
+
+        self._add_button.pack(side=tk.TOP)
+
+        self._body_widgets = dict()
+
+        self._headers = ["Strategy", "Contract", "Timeframe", "Balance %, TP %", "SL %"]
+
+        for idx, h in enumerate(self._headers):
+            header = tk.Label(self._table_frame, txt=h, bg=FG_COLOR, fg=FG_COLOR, font=BOLD_FONT)
+            header.grid(row=0, column=idx)
+
+        for h in self._headers:
+            self.body_widget.get[h] = dict()
+
+        self._body_index = 1
+
+    def _add_strategy_row(self):
+        return
--- interface/trades_component.py
+++ interface/trades_component.py
@@ -51,19 +51,18 @@ class TradesWatch(tk.Frame):
 
         self.body_widgets['side'][t_index].grid(row=b_index, column=4)
 
-
         self.body_widgets['quantity'][t_index] = tk.label(self._table_frame, text=data['quantity'], bg=BG_COLOR, fg=FG_COLOR_2, font=GLOBAL_FONT)
 
         self.body_widgets['quantity'][t_index].grid(row=b_index, column=5)
 
         self.body_widgets['status_var'][t_index] = tk.StringVar()
-        self.body_widgets['status'][t_index] = tk.label(self._table_frame, text=data['status'], bg=BG_COLOR, fg=FG_COLOR_2, font=GLOBAL_FONT)
+        self.body_widgets['status'][t_index] = tk.label(self._table_frame, textvariable=self._body_widgets['status_var'], bg=BG_COLOR, fg=FG_COLOR_2, font=GLOBAL_FONT)
 
         self.body_widgets['status'][t_index].grid(row=b_index, column=6)
 
         self.body_widgets['pnl_var'][t_index] = tk.StringVar()
-        self.body_widgets['pnl'][t_index] = tk.label(self._table_frame, text=data['status'], bg=BG_COLOR, fg=FG_COLOR_2, font=GLOBAL_FONT)
+        self.body_widgets['pnl_var'][t_index] = tk.label(self._table_frame, textvariable=self._body_widgets['pnl_var'], bg=BG_COLOR, fg=FG_COLOR_2, font=GLOBAL_FONT)
 
-        self.body_widgets['pnl'][t_index].grid(row=b_index, column=7)
+        self.body_widgets['pnl_var'][t_index].grid(row=b_index, column=7)
 
         self._body_index += 1
