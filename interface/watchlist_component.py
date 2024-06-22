8f053c7e2ba489143e55e3fc7e7b37c6fe7a0fda
softacoder <113185323+softacoder@users.noreply.github.com>
2024-06-22 02:14:13 +0100

    commit scrollable frames basic

--- interface/watchlist_component.py
+++ interface/watchlist_component.py
@@ -6,6 +6,7 @@ from models import *
 
 from interface.styling import *
 from interface.autocomplete_widget import Autocomplete
+# for interface.scrollable_frame import ScrollableFrame
 
 class Watchlist(tk.Frame):
     def __init__(self, binance_contracts: typing.Dict[str, Contract], bitmex_contracts: typing.Dict[str, Contract],
@@ -41,17 +42,28 @@ class Watchlist(tk.Frame):
 
         self._headers = ["symbol", "exchange", "bid", "ask", "remove"]
 
+        self._headers_frame = tk.Frame(self._table_frame, bg=BG_COLOR)
+
         for idx, h in enumerate(self._headers):
-            header = tk.Label(self._table_frame, text=h.capitalize() if h != "remove" else "", bg=BG_COLOR,
-                              fg=FG_COLOR, font=BOLD_FONT)
+            header = tk.Label(self._headers_frame, text=h.capitalize() if h != "remove" else "", bg=BG_COLOR,
+                              fg=FG_COLOR, font=GLOBAL_FONT, width=self._col_width)
             header.grid(row=0, column=idx)
+
+        header = tk.Label(self._headers_frame, text="", bg=BG_COLOR,
+                            fg=FG_COLOR, font=GLOBAL_FONT, width=2)
+        header.grid(row=0, column=len(self._headers))
+
+        self._headers_frame.pack(side=tk.TOP, anchor="nw")
+
+        self._body_frame = ScrollableFrame(self._table_frame, bg=BG_COLOR, height=250)
+        self._body_frame.pack(side=tk.TOP, fill=tk.X, anchor="nw")
 
         for h in self._headers:
             self.body_widgets[h] = dict()
             if h in ["bid", "ask"]:
                 self.body_widgets[h + "_var"] = dict()
 
-        self._body_index = 1
+        self._body_index = 0
 
     def _remove_symbol(self, b_index: int):
 
@@ -77,29 +89,29 @@ class Watchlist(tk.Frame):
 
         b_index = self._body_index
 
-        self.body_widgets['symbol'][b_index] = tk.Label(self._table_frame, text=symbol, bg=BG_COLOR, fg=FG_COLOR_2,
-                                                        font=GLOBAL_FONT)
+        self.body_widgets['symbol'][b_index] = tk.Label(self._body_frame.sub_frame, text=symbol, bg=BG_COLOR, fg=FG_COLOR_2,
+                                                        font=GLOBAL_FONT, width=self._col_width)
         self.body_widgets['symbol'][b_index].grid(row=b_index, column=0)
 
-        self.body_widgets['exchange'][b_index] = tk.Label(self._table_frame, text=exchange, bg=BG_COLOR, fg=FG_COLOR_2,
-                                                          font=GLOBAL_FONT)
+        self.body_widgets['exchange'][b_index] = tk.Label(self._body_frame.sub_frame, text=exchange, bg=BG_COLOR, fg=FG_COLOR_2,
+                                                          font=GLOBAL_FONT, width=self._col_width)
         self.body_widgets['exchange'][b_index].grid(row=b_index, column=1)
 
         self.body_widgets['bid_var'][b_index] = tk.StringVar()
-        self.body_widgets['bid'][b_index] = tk.Label(self._table_frame,
+        self.body_widgets['bid'][b_index] = tk.Label(self._body_frame.sub_frame,
                                                      textvariable=self.body_widgets['bid_var'][b_index],
-                                                     bg=BG_COLOR, fg=FG_COLOR_2, font=GLOBAL_FONT)
+                                                     bg=BG_COLOR, fg=FG_COLOR_2, font=GLOBAL_FONT,width=self._col_width)
         self.body_widgets['bid'][b_index].grid(row=b_index, column=2)
 
         self.body_widgets['ask_var'][b_index] = tk.StringVar()
-        self.body_widgets['ask'][b_index] = tk.Label(self._table_frame,
+        self.body_widgets['ask'][b_index] = tk.Label(self._body_frame.sub_frame,
                                                      textvariable=self.body_widgets['ask_var'][b_index],
-                                                     bg=BG_COLOR, fg=FG_COLOR_2, font=GLOBAL_FONT)
+                                                     bg=BG_COLOR, fg=FG_COLOR_2, font=GLOBAL_FONT, width=self._col_width)
         self.body_widgets['ask'][b_index].grid(row=b_index, column=3)
 
-        self.body_widgets['remove'][b_index] = tkmac.Button(self._table_frame, text="X", borderless=True,
+        self.body_widgets['remove'][b_index] = tkmac.Button(self._body_frame.sub_frame, text="X",
                                                          bg="darkred", fg=FG_COLOR, font=GLOBAL_FONT,
-                                                         command=lambda: self._remove_symbol(b_index))
+                                                         command=lambda: self._remove_symbol(b_index), width=4)
         self.body_widgets['remove'][b_index].grid(row=b_index, column=4)
 
         self._body_index += 1
