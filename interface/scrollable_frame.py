a71515d770e603a664fa0fad422a5723b0a59e38
softacoder <113185323+softacoder@users.noreply.github.com>
2024-06-23 02:37:39 +0100

    commit scrollable frames complex

--- interface/scrollable_frame.py
+++ interface/scrollable_frame.py
@@ -9,6 +9,8 @@ class ScrollableFrame(tk.Frame):
         self.sub_frame = tk.Frame(self.canvas, **kwargs)
 
         self.sub_frame.bind("<Configure>", self._on_frame_configure)
+        self.sub_frame.bind("<Enter>", self._activate_mousewheel)
+        self.sub_frame.bind("<Leave>", self._deactivate_mousewheel)
 
         self.canvas.create_window((0, 0), window=self.sub_frame, anchor="nw")
 
@@ -19,3 +21,12 @@ class ScrollableFrame(tk.Frame):
 
     def _on_frame_configure(self, event: tk.Event):
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+    def _activate_mousewheel(self, event: tk.Event):
+        self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
+
+    def _activate_mousewheel(self, event: tk.Event):
+        self.canvas.unbind_all("<MouseWheel>")
+
+    def _activate_mousewheel(self, event: tk.Event):
+        self.canvas.yview_scroll(int(-1 * (event.delta / 60)), "units")
