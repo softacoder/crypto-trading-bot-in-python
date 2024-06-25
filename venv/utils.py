52ad7cab4c117bea80b1ee2adea5c9dbd9306463
softacoder <113185323+softacoder@users.noreply.github.com>
2024-06-25 17:45:46 +0100

    commit as changes
    # Source code provided

--- venv/utils.py
+++ venv/utils.py
@@ -26,3 +26,48 @@ def check_float_format(text: str):
         
     else:
         return False
+    
+
+    # Source code to compare. Remember the specific changes for macos
+#     def check_integer_format(text: str) -> bool:
+
+#     """
+#     Check that the text is a positive integer
+#     :param text: Text entered by the user in a tk.Entry widget
+#     :return:
+#     """
+
+#     if text == "":
+#         return True
+
+#     if all(x in "0123456789" for x in text):
+#         try:
+#             int(text)
+#             return True
+#         except ValueError:
+#             return False
+
+#     else:
+#         return False
+
+
+# def check_float_format(text: str) -> bool:
+
+#     """
+#     Check that the text is a positive floating number
+#     :param text: Text entered by the user in a tk.Entry widget
+#     :return:
+#     """
+
+#     if text == "":
+#         return True
+
+#     if all(x in "0123456789." for x in text) and text.count(".") <= 1:
+#         try:
+#             float(text)
+#             return True
+#         except ValueError:
+#             return False
+
+#     else:
+#         return False
